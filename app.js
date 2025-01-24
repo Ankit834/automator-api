575a12729c3965265f5d0c3e9cb2f31e8be4f246
Ankit Sharma <ankit.sharma@postman.com@Ankits-MacBook-Pro.local>
2025-01-24 14:14:30 +0530

    Add automation at server support

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const puppeteer = require('puppeteer');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const Script = require('./models/script.model.js');
@@ -20,6 +21,90 @@ app.use('/api/scripts', scriptRoute);
 app.get('/', (req, res) => {
   res.send('Hello from Node API Server Updated');
 });
+
+// Endpoint to execute recorded scripts
+app.post('/execute-script', async (req, res) => {
+  const { url, recordedEvents } = req.body;
+  console.log('ANKIT: check', url, recordedEvents);
+
+  if (!url || !Array.isArray(recordedEvents)) {
+    return res.status(400).send({
+      error: "Invalid request. 'url' and 'recordedEvents' are required.",
+    });
+  }
+
+  try {
+    // Launch Puppeteer
+    const browser = await puppeteer.launch({
+      headless: false, // Set to false to see browser actions
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+
+    // Navigate to the provided URL
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    // Replay the recorded events
+    for (const event of recordedEvents) {
+      await new Promise((resolve) => setTimeout(resolve, 1500));
+      await replayEvent(page, event);
+    }
+
+    await browser.close();
+    res.send({ status: 'success', message: 'Script executed successfully.' });
+  } catch (error) {
+    console.error('Error executing script:', error);
+    res.status(500).send({ status: 'error', message: error.message });
+  }
+});
+
+const getSelector = (step) => {
+  if (step.testId) {
+    return `[data-testid=${step.testId}]`;
+  }
+  if (step.id) {
+    return `#${step.id}`;
+  }
+  if (step.target) {
+    return step.target;
+  }
+  if (step.className) {
+    return `${step.className}`;
+  }
+
+  return null;
+};
+
+// Function to replay events
+const replayEvent = async (page, event) => {
+  const { type, value } = event;
+
+  const selector = getSelector(event);
+
+  // Wait for the element to be available
+  await page.waitForSelector(selector, { timeout: 5000 });
+
+  switch (type) {
+    case 'click':
+      await page.click(selector);
+      break;
+
+    case 'input':
+      await page.$eval(
+        selector,
+        (input, text) => {
+          input.value = text; // Set the input value directly
+          input.dispatchEvent(new Event('input', { bubbles: true })); // Optional: trigger event
+        },
+        value || ''
+      );
+      break;
+
+    default:
+      console.warn(`Event type "${type}" is not supported.`);
+  }
+};
 
 mongoose
   .connect(process.env.MONGO_URL)
